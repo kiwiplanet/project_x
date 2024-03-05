3cba0ade918deeed9f323456cb5f4a56b038b6f0
kiwiplanet <149170712+kiwiplanet@users.noreply.github.com>
2024-03-05 17:08:44 +0900

    登録項目の追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,21 @@
                             </div>
                         </div>
 
+                        <!-- 社員番号の登録機能を追加 -->
+                        <div class="row mb-3">
+                            <label for="staff_id" class="col-md-4 col-form-label text-md-end">{{ __('社員番号') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="staff_id" type="text" class="form-control @error('staff_id') is-invalid @enderror" name="staff_id" value="{{ old('staff_id') }}" required autocomplete="staff_id" autofocus>
+
+                                @error('staff_id')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
